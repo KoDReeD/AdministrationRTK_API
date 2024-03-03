9d04fbcfb9cd2810ca31ebaffb33947114b49811
kseniaPractice <kseniavolkova555@gmail.com>
2024-03-03 23:04:29 +0300

    Создание user на Gitea

--- server-asp-api/Controllers/RegistrationController.cs
+++ server-asp-api/Controllers/RegistrationController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using server_asp_api.Models;
 using server_asp_api.Models.Gitea;
 using server_asp_api.Services;
@@ -12,26 +14,51 @@ namespace server_asp_api.Controllers;
 public class RegistrationController : ControllerBase
 {
     [HttpPost]
-    public async void RegistrationOnGitea(GiteaRegistrationModel data)
+    public async Task<ResultModel> RegistrationOnGitea(GiteaRegistrationModel data)
     {
-        using HttpClient httpClient = new HttpClient();
-        
-        string json = JsonConvert.SerializeObject(data);
-        StringContent content = new StringContent(json);
-        var url = "host/api/v1/admin/users";
-        
-        using var response = await httpClient.PostAsync(url, content);
-        
-        if (response.IsSuccessStatusCode)
+        string adminUsername = "";
+        string adminPassword = "";
+        string base64Credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(adminUsername + ":" + adminPassword));
+        var url = "http://localhost:3031/api/v1/admin/users";
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        using (HttpClient client = new HttpClient())
         {
-            string responseContent = await response.Content.ReadAsStringAsync();
-        }
-        else
-        {
-            
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                var obj = JsonConvert.DeserializeObject<GiteaUserObject>(responseContent);
+                var responseData = new ResultModel()
+                {
+                    Data = obj,
+                    Message = "успех",
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+                return responseData;
+            }
+            else
+            {
+                string responseContent = response.Content.ReadAsStringAsync().Result;
+                GiteaBadResponse dataJson = JsonConvert.DeserializeObject<GiteaBadResponse>(responseContent);
+                var responseData = new ResultModel()
+                {
+                    Data = dataJson,
+                    Message = "ошибка",
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+                return responseData;
+            }
         }
     }
+
     
+
     //TODO: пофиксить отображение всех бд в MSsql и ролей (если можно)
 
     [HttpPost]
@@ -39,25 +66,25 @@ public class RegistrationController : ControllerBase
     {
         var connectionString = "Host=localhost;Username=postgres;Password=admin;";
         PostgreDatabaseManager manager = new PostgreDatabaseManager();
-        var result = await manager.Register(username,password, connectionString);
+        var result = await manager.Register(username, password, connectionString);
         return Ok(result);
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Mssql(string username, string password)
     {
         var connectionString = "Server=localhost;User Id=admin;Password=admin;Trusted_Connection=True;";
         MssqlDatabaseManager manager = new MssqlDatabaseManager();
-        var result = await manager.Register(username,password, connectionString);
+        var result = await manager.Register(username, password, connectionString);
         return Ok(result);
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Mysql(string username, string password)
     {
         var connectionString = "server=localhost;user=root;port=3306;password=admin;Allow User Variables=true;";
         MysqlDatabaseNanager manager = new MysqlDatabaseNanager();
-        var result = await manager.Register(username,password, connectionString);
+        var result = await manager.Register(username, password, connectionString);
         return Ok(result);
     }
 }
---
+++ server-asp-api/Models/Gitea/GiteaResponseModels.cs
@@ -0,0 +1,32 @@
+﻿namespace server_asp_api.Models.Gitea;
+
+public class GiteaBadResponse
+{
+    public string Message { get; set; }
+    public string Url { get; set; }
+}
+    
+public class GiteaUserObject
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string LoginName { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string AvatarUrl { get; set; }
+    public string Language { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime LastLogin { get; set; }
+    public DateTime Created { get; set; }
+    public bool Restricted { get; set; }
+    public bool Active { get; set; }
+    public bool ProhibitLogin { get; set; }
+    public string Location { get; set; }
+    public string Website { get; set; }
+    public string Description { get; set; }
+    public string Visibility { get; set; }
+    public int FollowersCount { get; set; }
+    public int FollowingCount { get; set; }
+    public int StarredReposCount { get; set; }
+    public string Username { get; set; }
+}
